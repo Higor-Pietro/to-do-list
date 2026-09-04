032433972b89cbe2bcda13f60556ede453a84d2f
Higor-Pietro <medeiroshigor2018@gmail.com>
2026-09-04 11:29:01 -0300

    feat: adiciona métodos restantes no repository

--- Back-end/higor/src/main/java/com/todolist/repository/TaskRepository.java
+++ Back-end/higor/src/main/java/com/todolist/repository/TaskRepository.java
@@ -32,16 +32,65 @@ public class TaskRepository {
         }
     }
 
-    public void MarcarTask(){
+    public void MarcarTask(TaskDTO task){
+
+        String sql = "UPDATE tasks SET feito = 1 WHERE nome = ?";
+
+         try (Connection connection = ConnectionFactory.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+                System.out.println("Conexão Feita");
+
+            statement.setString(1, task.nome());
+            
+
+            statement.executeUpdate();
+
+            System.out.println("Tarefa adicionada em DataBase");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
 
     }
 
-    public void desmarcarTask(){
+    public void desmarcarTask(TaskDTO task){
+
+        String sql = "UPDATE tasks SET feito = 0 WHERE nome = ?";
+
+         try (Connection connection = ConnectionFactory.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+                System.out.println("Conexão Feita");
+
+            statement.setString(1, task.nome());
+          
+
+            statement.executeUpdate();
+
+            System.out.println("Tarefa adicionada em DataBase");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public void ListarTask(){
+    public void ListarTask(TaskDTO task){
+
+        String sql = "select t.nome, t.descricao from tasks t";
+
+         try (Connection connection = ConnectionFactory.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.executeUpdate();
+
+            System.out.println("Tarefa adicionada em DataBase");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
